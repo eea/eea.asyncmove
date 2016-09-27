1034c1e4fdba2d93abdf3a69369a68a15c83083e
ichim-david <ichim.david@gmail.com>
2016-09-27 12:55:48 +0300

    Refs #75296 register IAsyncRename interfaces

--- eea/asyncmove/events/interfaces.py
+++ eea/asyncmove/events/interfaces.py
@@ -7,21 +7,26 @@ class IAsyncMoveEvent(IObjectEvent):
     """ Base Event Interface for all Async events
     """
 
+
 class IAsyncMoveSuccess(IAsyncMoveEvent):
     """ Async job for move succeeded
     """
+
 
 class IAsyncMoveFail(IAsyncMoveEvent):
     """ Async job for move failed
     """
 
+
 class IAsyncRenameSuccess(IAsyncMoveEvent):
     """ Async job for rename succeeded
     """
 
+
 class IAsyncRenameFail(IAsyncMoveEvent):
     """ Async job for rename failed
     """
+
 
 class IAsyncMoveSaveProgress(IAsyncMoveEvent):
     """ Async job for save move progress
