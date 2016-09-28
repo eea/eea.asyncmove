8377a35c4f3f9a86325b72ed232d16fcb979c62f
ichim-david <ichim.david@gmail.com>
2016-09-28 11:56:46 +0300

    Refs #75296 uninstall now removes content rules

--- eea/asyncmove/Extensions/install.py
+++ eea/asyncmove/Extensions/install.py
@@ -1,6 +1,10 @@
 """ Uninstall Profile
 """
 from Products.CMFCore.utils import getToolByName
+from plone.app.contentrules import api
+from plone.contentrules.engine.interfaces import IRuleStorage
+from zope.component import queryUtility
+
 
 def uninstall(portal, reinstall=False):
     """ Uninstall profile setup
@@ -9,4 +13,23 @@ def uninstall(portal, reinstall=False):
         setup_tool = getToolByName(portal, 'portal_setup')
         setup_tool.runAllImportStepsFromProfile(
             'profile-eea.asyncmove:uninstall')
+        remove_content_rules(portal)
         return "Ran all uninstall steps."
+
+
+def remove_content_rules(portal):
+    """ Remove content rules on uninstall
+    """
+    storage = queryUtility(IRuleStorage)
+    if not storage:
+        return
+    ids = ["eea-asyncmove-rule-fail", "eea-asyncmove-rule-success",
+           "eea-asyncmove-rename-rule-fail",
+           "eea-asyncmove-rename-rule-success"]
+    for i in ids:
+        found = storage.get(i)
+        if found:
+            api.unassign_rule(portal, i)
+            del storage[i]
+
+
